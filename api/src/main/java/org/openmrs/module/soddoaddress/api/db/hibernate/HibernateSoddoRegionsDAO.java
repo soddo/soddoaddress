f30981767aaa70df3d46ed298a86af69cb4df501
judywawira <judywawira@gmail.com>
2014-08-27 10:45:03 -0400

    Update HibernateSoddoRegionsDAO.java

--- api/src/main/java/org/openmrs/module/soddoaddress/api/db/hibernate/HibernateSoddoRegionsDAO.java
+++ api/src/main/java/org/openmrs/module/soddoaddress/api/db/hibernate/HibernateSoddoRegionsDAO.java
@@ -3,35 +3,37 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.openmrs.module.soddoaddress.SoddoRegions;
 import org.openmrs.module.soddoaddress.api.db.SoddoRegionsDAO;
-public class SoddoRegionsDAOImpl implements SoddoRegionsDAO {
-private SessionFactory sessionFactory;
-/**
-* @param sessionFactory the sessionFactory to set
-*/
-public void setSessionFactory(SessionFactory sessionFactory) {
-this.sessionFactory = sessionFactory;
-}
-/**
-* @return the sessionFactory
-*/
-public SessionFactory getSessionFactory() {
-return sessionFactory;
-}
-@Override
-public List<SoddoRegions> getAllRegions() {
-return sessionFactory.getCurrentSession().createCriteria(SoddoRegions.class).list();
-}
-@Override
-public SoddoRegions getRegion(Integer id) {
-return (SoddoRegions) sessionFactory.getCurrentSession().get(SoddoRegions.class, id);
-}
-@Override
-public SoddoRegions saveRegion(SoddoRegions soddoRegions) {
-sessionFactory.getCurrentSession().save(soddoRegions);
-return soddoRegions;
-}
-@Override
-public void purgeRegion(SoddoRegions soddoRegions) {
-sessionFactory.getCurrentSession().delete(soddoRegions);
-}
+
+
+public class HibernateSoddoRegionsDAO  implements SoddoRegionsDAO {
+        private SessionFactory sessionFactory;
+        /**
+        * @param sessionFactory the sessionFactory to set
+        */
+        public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        }
+        /**
+        * @return the sessionFactory
+        */
+        public SessionFactory getSessionFactory() {
+        return sessionFactory;
+        }
+        @Override
+        public List<SoddoRegions> getAllRegions() {
+        return sessionFactory.getCurrentSession().createCriteria(SoddoRegions.class).list();
+        }
+        @Override
+        public SoddoRegions getRegion(Integer id) {
+        return (SoddoRegions) sessionFactory.getCurrentSession().get(SoddoRegions.class, id);
+        }
+        @Override
+        public SoddoRegions saveRegion(SoddoRegions soddoRegions) {
+        sessionFactory.getCurrentSession().save(soddoRegions);
+        return soddoRegions;
+        }
+        @Override
+        public void purgeRegion(SoddoRegions soddoRegions) {
+        sessionFactory.getCurrentSession().delete(soddoRegions);
+        }
 }
